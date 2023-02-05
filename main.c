92f372e944b146667e718226340046b74ecc25cd
fedi-nabli <fedinabli@gmail.com>
2023-02-05 18:20:50 +0100

    Finished preparing the project

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include "helpers/vector.h"
+#include "compiler.h"
 
 int main()
 {
-  printf("Hello world\n");
+  int res = compile_file("./test.c", "./test", 0);
+  if (res == COMPILER_FILE_COMPILED_OK)
+  {
+    printf("Everything compiled fine\n");
+  }
+  else if (res == COMPILER_FAILED_WITH_ERROR)
+  {
+    printf("Compile failed\n");
+  }
+  else
+  {
+    printf("Unknown response for compile file\n");
+  }
   return 0;
 }
