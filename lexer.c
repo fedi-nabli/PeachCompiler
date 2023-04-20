cd8658932aecdb4045b2073a2ab9a6bf82de00b9
fedi-nabli <fedinabli@gmail.com>
2023-04-20 01:00:15 +0100

    Implmenting datatypes and keywords - Part 1

--- lexer.c
+++ lexer.c
@@ -295,6 +295,19 @@ bool lex_is_in_expression()
   return lex_process->current_expression_count > 0;
 }
 
+bool keyword_is_datatype(const char* str)
+{
+  return S_EQ(str, "void") ||
+          S_EQ(str, "char") ||
+          S_EQ(str, "int") ||
+          S_EQ(str, "short") ||
+          S_EQ(str, "float") ||
+          S_EQ(str, "double") ||
+          S_EQ(str, "long") ||
+          S_EQ(str, "struct") ||
+          S_EQ(str, "union");
+}
+
 bool is_keyword(const char* str)
 {
   return S_EQ(str, "unsigned") ||
